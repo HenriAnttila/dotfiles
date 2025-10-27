f61a9668fa48bdf5da871c6eaf994c646e76e804
Henri Anttila <henri@ikius.com>
2025-10-27 18:08:27 +0200

    initial commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Symlink Neovim config
+ln -sf ~/dotfiles/nvim ~/.config/nvim
+
+# Add other symlinks as needed
+# ln -sf ~/dotfiles/bashrc ~/.bashrc
+# ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+
+echo "Dotfiles installed!"
---
+++ nvim/nvim/lua/plugins/theme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+}
