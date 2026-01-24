340550574e14aacd192daec4250b97e34ebba7c7
Henri Anttila <henri@ikius.com>
2026-01-24 22:13:41 +0200

    cleanup themes

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -7,66 +7,12 @@ local function applySnackStyling()
 end
 
 return {
-  -- Gruvbox Material Original with custom background
   {
-    "sainnhe/gruvbox-material",
+    "datsfilipe/vesper.nvim",
+    priority = 1005,
     lazy = false,
-    priority = 1001,
     config = function()
-      vim.g.gruvbox_material_background = "hard" -- soft, medium, hard
-      vim.g.gruvbox_material_foreground = "original"
-      vim.g.gruvbox_material_enable_bold = 1
-      vim.g.gruvbox_material_enable_italic = 1
-
-      -- Apply colorscheme FIRST
-      vim.cmd([[colorscheme gruvbox-material]])
-
-      -- Then override the background with custom color
-      vim.cmd([[highlight Normal guibg=#0e0f17 ctermbg=NONE]])
-      vim.cmd([[highlight NormalNC guibg=#0e0f17 ctermbg=NONE]]) -- Non-current windows
-    end,
-  },
-  {
-    "tiesen243/vercel.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("vercel").setup({
-        theme = "dark",
-        transparent = false,
-        italics = {
-          comments = true,
-          keywords = true,
-          functions = true,
-          strings = true,
-          variables = true,
-          bufferline = false,
-        },
-        overrides = {},
-      })
-
-      vim.cmd.colorscheme("vercel")
-      applySnackStyling()
-    end,
-  },
-  {
-    "AlexvZyl/nordic.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "Mofiqul/vscode.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  { "EdenEast/nightfox.nvim", lazy = false, priority = 1000
- },
-  {
-    "vague-theme/vague.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1100, -- make sure to load this before all the other plugins
-    config = function()
-      vim.cmd("colorscheme vague")
+      vim.cmd("colorscheme vesper")
       applySnackStyling()
     end,
   },
