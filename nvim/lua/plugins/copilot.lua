70a3c4cf1ca708c2cb2d68151a2fc61ab893f7d5
Henri Anttila <henri@ikius.com>
2026-02-02 15:24:53 +0200

    feat: rebind tab complete to ctrl+y

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,10 +1,33 @@
 return {
   {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        ["<Tab>"] = {
+          LazyVim.cmp.map({ "snippet_forward" }),
+          "fallback",
+        },
+        ["<C-y>"] = {
+          LazyVim.cmp.map({ "ai_accept" }),
+          "fallback",
+        },
+      },
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         copilot = {
           keys = {
+            {
+              "<C-y>",
+              function()
+                vim.lsp.inline_completion.accept()
+              end,
+              desc = "Accept Copilot Suggestion",
+              mode = "i",
+            },
             {
               "<PageDown>",
               function()
