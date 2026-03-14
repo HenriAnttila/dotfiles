77d7deef0ee4c206f3a526194b4212d8ed7aba72
Henri Anttila <henkkazor@gmail.com>
2026-03-14 13:24:25 +0200

    sync terminal background w nvim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,35 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Sync terminal background with Neovim colorscheme (hides padding)
+local function set_bg(color)
+  local hex = string.format("#%06x", color)
+  if vim.env.TMUX then
+    io.write(string.format("\027Ptmux;\027\027]11;%s\007\027\\", hex))
+    vim.fn.system({ "tmux", "set", "-g", "status-style", "bg=" .. hex })
+  else
+    io.write(string.format("\027]11;%s\027\\", hex))
+  end
+end
+
+local function reset_bg()
+  if vim.env.TMUX then
+    io.write("\027Ptmux;\027\027]111;\007\027\\")
+    vim.fn.system({ "tmux", "set", "-g", "status-style", "bg=#2E3440" })
+  else
+    io.write("\027]111\027\\")
+  end
+end
+
+vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+  callback = function()
+    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+    if not normal.bg then return end
+    set_bg(normal.bg)
+  end,
+})
+
+vim.api.nvim_create_autocmd("UILeave", {
+  callback = function() reset_bg() end,
+})
